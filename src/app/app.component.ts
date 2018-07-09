04d1ac611583e31204d9e238ae3df3edf54a70e1
Michael Trinkle <mtrinkle11@yahoo.com>
2018-07-09 16:03:02 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,41 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  todoInput: string = "";
+
+  todos = [];
+
+  createTodo() {
+    let trimmedInput = this.todoInput.trim();
+    console.log(this.todoInput);
+    if(trimmedInput !="") {
+      this.todos.push(this.todoInput);
+    }
+    // code that will create a todo
+    this.todoInput = "";
+
+  }
+
+  editTodo(todo) {
+    // edits the todo
+    // rename that input to whatever I write
+    let index = this.todos.indexOf(todo);
+    console.log(index);
+
+    this.todos[index] = prompt("Edit Now",this.todos[index]);
+  }
+
+  deleteTodo(todo) {
+    // deletes todo
+    let index = this.todos.indexOf(todo);
+    this.todos.splice(index, 1);
+    // console.log(this.deleteTodo(todo));
+
+  }
+
+  deleteAll(todo) {
+    this.todos = [];
+  }
 }
