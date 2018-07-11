43851d992f55656bff5bd55b60c16c8b8344ce9a
Michael Trinkle <mtrinkle11@yahoo.com>
2018-07-11 12:09:08 -0500

    Small changes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from "@angular/router";
+
+import { AboutComponent } from './about/about.component';
+
+const routes: Routes = [
+  {
+    path: "about",
+    component: "AboutComponent"  
+  },
+  {
+    path: "",
+    redirectTo: "/",
+    pathMatch: "full"
+  }
+]
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
